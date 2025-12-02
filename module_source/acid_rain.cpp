f5f5a5f8deee6002e3e718e3ef0d2b5c24858ed3
pehilbert <pehilbert03@gmail.com>
2025-12-01 17:06:40 -0700

    Implement acid rain state and basic configuration values

--- module_source/acid_rain.cpp
+++ module_source/acid_rain.cpp
@@ -1,13 +1,70 @@
 #include "acid_rain.h"
+#include "core/object/class_db.h"
 
 void AcidRainManager::_bind_methods() {
-	// bind methods/properties here if needed
+	// Bind setters/getters so they appear in the editor and can be used from scripts
+	ClassDB::bind_method(D_METHOD("set_rain_seconds", "seconds"), &AcidRainManager::setRainSeconds);
+	ClassDB::bind_method(D_METHOD("get_rain_seconds"), &AcidRainManager::getRainSeconds);
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "rain_seconds", PROPERTY_HINT_RANGE, "0.1,3600,0.1"), "set_rain_seconds", "get_rain_seconds");
+
+	ClassDB::bind_method(D_METHOD("set_clear_seconds", "seconds"), &AcidRainManager::setClearSeconds);
+	ClassDB::bind_method(D_METHOD("get_clear_seconds"), &AcidRainManager::getClearSeconds);
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "clear_seconds", PROPERTY_HINT_RANGE, "0.1,3600,0.1"), "set_clear_seconds", "get_clear_seconds");
+
+	ClassDB::bind_method(D_METHOD("set_warning_time", "seconds"), &AcidRainManager::setWarningTime);
+	ClassDB::bind_method(D_METHOD("get_warning_time"), &AcidRainManager::getWarningTime);
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "warning_time", PROPERTY_HINT_RANGE, "0,600,0.1"), "set_warning_time", "get_warning_time");
 }
 
 // Constructor
 AcidRainManager::AcidRainManager() {
+	// sensible defaults so new nodes have usable values in the editor
+	rainSeconds = 30.0f;
+	clearSeconds = 60.0f;
+	warningTime = 5.0f;
+	currentState = AcidRainState::Clear;
 }
 
 // Destructor
 AcidRainManager::~AcidRainManager() {
+}
+
+// Setters / getters
+void AcidRainManager::setRainSeconds(float p_rainSeconds) {
+	if (p_rainSeconds < 0.0f) {
+		p_rainSeconds = 0.0f;
+	}
+	rainSeconds = p_rainSeconds;
+}
+
+float AcidRainManager::getRainSeconds() {
+	return rainSeconds;
+}
+
+void AcidRainManager::setClearSeconds(float p_clearSeconds) {
+	if (p_clearSeconds < 0.0f) {
+		p_clearSeconds = 0.0f;
+	}
+	clearSeconds = p_clearSeconds;
+}
+
+float AcidRainManager::getClearSeconds() {
+	return clearSeconds;
+}
+
+void AcidRainManager::setWarningTime(float p_warningTime) {
+	if (p_warningTime < 0.0f) {
+		p_warningTime = 0.0f;
+	}
+
+	// only allow warning times less than the amount of clear time
+	if (p_warningTime < clearSeconds) {
+		warningTime = p_warningTime;
+	} else {
+		warningTime = clearSeconds;
+	}
+}
+
+float AcidRainManager::getWarningTime() {
+	return warningTime;
 }
--- module_source/acid_rain.h
+++ module_source/acid_rain.h
@@ -1,11 +1,26 @@
 #include "scene/2d/node_2d.h"
 
+enum AcidRainState {
+	Raining, Clear
+};
+
 class AcidRainManager : public Node2D {
 	GDCLASS(AcidRainManager, Node2D);
 
 public:
 	AcidRainManager();
 	~AcidRainManager();
+	void setRainSeconds(float p_rainSeconds);
+	float getRainSeconds();
+	void setClearSeconds(float p_clearSeconds);
+	float getClearSeconds();
+	void setWarningTime(float p_warningTime);
+	float getWarningTime();
+
+	float rainSeconds;
+	float clearSeconds;
+	float warningTime;
+	AcidRainState currentState;
 
 protected:
 	static void _bind_methods();
